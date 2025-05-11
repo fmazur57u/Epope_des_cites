934f1ca7daff3e67a571b23cab4f34d349c9afde
fmazur57u <florianmazur2@gmail.com>
2025-05-11 15:50:17 +0200

    Creation de la fonction load_json

--- Projet_Epopée_des_cité.py
+++ Projet_Epopée_des_cité.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Dict, Union
+import json
 
 
 class Joueur:
@@ -19,3 +20,54 @@ class Joueur:
         self.inventaire = inventaire
         self.points_de_vie = points_de_vie
         self.force = force
+
+
+def load_json(
+    filename: str,
+) -> Dict[str, List[Dict[str, Union[str, List[str], int]]]]:
+    """Fonction qui permet de charger des données à partir d'un fichier json.
+
+    Args:
+        filename (str): Le nom du fichier à partir duquel il faut charger les données.
+
+    Returns:
+        Dict[str, List[Dict[str, Union[str, List[str], int]]]]: Un dictionnaire qui contient
+        trois liste de dictionnaire, le premier pour les lieux, le deuxiéme pour les
+        personnages et le troisiéme pour les ressources.
+
+    Raises:
+        FileNotFoundError: Quand le fichier n'a pas été trouver.
+        JSONDecodeError: Si il y a une erreur dans le json du fichier charger.
+
+    Exemples:
+        >>> data = load_json(data.json)
+        >>> print(data)
+        {
+            "lieux": [
+                {"nom": "Temple Oublié", "description": "Un temple envahi par la végétation", "ressources": ["or", "pierres"], "ennemis": ["serpent géant"]},
+                {"nom": "Forêt Maudite", "description": "Une forêt sombre et mystérieuse", "ressources": ["bois", "herbes"], "ennemis": ["loup spectral"]}
+            ],
+            "personnages": [
+                {"nom": "Arwen", "type": "allié", "force": 5, "dialogue": "Je peux t'aider à explorer, mais il me faut 10 unités d'or."},
+                {"nom": "Serpent Géant", "type": "ennemi", "force": 8, "dialogue": "Sssss... Tu ne passeras pas !"}
+            ],
+            "ressources": [
+                {"nom": "or", "quantite": 20, "utilite": "Acheter de l'aide"},
+                {"nom": "bois", "quantite": 10, "utilite": "Construire des abris"}
+            ]
+        }
+    """
+    try:
+        with open(file=filename, mode="r", encoding="utf-8") as fichier:
+            data = json.load(fichier)
+    except FileNotFoundError as e:
+        print(f"Le fichier {filename} n'a pas été trouver.")
+        return {}
+    except json.JSONDecodeError as e:
+        print(f"Erreur de décodage : {e}")
+        return {}
+    return data
+
+
+data = load_json("data.json")
+print(data)
